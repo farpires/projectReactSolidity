ee6bef89c2c0b0030b0a4ffade4a1092fb470814
Fabio Arpires <arpirez.fabio@gmail.com>
2021-01-20 23:47:56 -0300

    Item contract finished

--- Contracts/ItemManager.sol
+++ Contracts/ItemManager.sol
@@ -1,15 +1,15 @@
 //  SPDX-License-Identifier: MIT
 pragma solidity >0.6.99 <0.8.1; // pragma solidity ^0.7.0;
 
+import "./Item.sol";
+
 contract ItemManager{
-
-
     enum SupplyChainSteps{Created, Paid, Delivered}
 
     struct S_Item {
+        Item _item;// uint _priceInWei;
         ItemManager.SupplyChainSteps _step; //enum
         string _identifier;
-        uint _priceInWei;
     }
 
     mapping(uint => S_Item) public items; // formara una lista
@@ -19,7 +19,7 @@ contract ItemManager{
     _itemIndex: identificador del objeto
     _step: el paso en el que se encuentra el item enum
     */
-    event SupplyChainStep(uint _itemIndex, uint _step);
+    event SupplyChainStep(uint _itemIndex, uint _step, address _address);
 
     /*
     CREATED:
@@ -28,11 +28,12 @@ contract ItemManager{
      emit SupplyChainStep(p1,p2) emitira el oobjeto se creara con el index 1 incrementando
     */
     function createItem(string memory _identifier, uint _priceInWei ) public {
-        items[index]._priceInWei = _priceInWei;
+        Item item = new Item(this, _priceInWei, index);
+        items[index]._item = item;
         items[index]._step = SupplyChainSteps.Created;
         items[index]._identifier = _identifier;
         
-        emit SupplyChainStep(index, uint(items[index]._step));
+        emit SupplyChainStep(index, uint(items[index]._step), address(item));
 
         index++;
     }
@@ -45,11 +46,14 @@ contract ItemManager{
         _SI LAS DOS verificacion: son exitosa se cambia el paso de  CREATED A PAID
     */
     function triggerPayment(uint _index) public payable {
-        require(items[index]._priceInWei == msg.value, "not fully paid");
+        Item item = items[_index]._item;
+        require(address(item)== msg.sender,"Only items are allowed to update themselves");
+        require(item.priceInWei() == msg.value, "not fully paid yet");
         require(items[index]._step == SupplyChainSteps.Created,
         "Item is further in the supply chain");
+        
         items[_index]._step = SupplyChainSteps.Paid;
-        emit SupplyChainStep(_index,uint(items[_index]._step));
+        emit SupplyChainStep(_index,uint(items[_index]._step), address(item));
     }
 
     /*
@@ -61,8 +65,9 @@ contract ItemManager{
         require(items[_index]._step == SupplyChainSteps.Paid,
         "Item is further in the supply chain"
          );
+
           items[_index]._step = SupplyChainSteps.Delivered;
-        emit SupplyChainStep(_index,uint(items[_index]._step));
+        emit SupplyChainStep(_index,uint(items[_index]._step), address(items[_index]._item));
     }
 
 
